adf56337523818b575446750b822d503a645b541
kenkz447 <kenkz447@gmail.com>
2018-06-06 14:16:22 +0700

    update app port

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function doCreateApp() {
 	registerGetJWTRoute(app)
 	registerRegisterRoute(app)
 
-	app.listen(3000, () => {
+	app.listen(process.env.PORT || 3000, () => {
 		console.log('INFO: Express server is running.')
 	})
 }
