f6c55cb3287aa6bc0e58b4ec1646adf9f104609d
Tung Nguyen <kenkz447@gmail.com>
2018-06-12 17:50:47 +0700

    update

--- src/graphql/type/user/user.js
+++ src/graphql/type/user/user.js
@@ -1,57 +1,66 @@
-
 import {
-	GraphQLObjectType,
-	GraphQLString,
-	GraphQLID
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLID
 } from 'graphql/type'
 
 import { userCreate, userDelete, userGet } from '@/mongoose'
 
 import {
-	graphQLPage,
-	graphQLPageArgs,
-	GraphQLPageArgs
+    graphQLPage,
+    graphQLPageArgs,
+    GraphQLPageArgs
 } from '../pagination'
 
 import { fieldCreate } from '../../utilities'
 
+export const userNodeType = new GraphQLObjectType({
+    name: 'user',
+    description: 'User item',
+    fields: {
+        _id: fieldCreate(GraphQLID),
+        username: fieldCreate(GraphQLString),
+        email: fieldCreate(GraphQLString)
+    }
+})
+
 export const userType = new GraphQLObjectType({
-	name: 'user',
-	description: 'User item',
-	fields: {
-		_id: fieldCreate(GraphQLID),
-		username: fieldCreate(GraphQLString),
-		email: fieldCreate(GraphQLString)
-	}
+    name: 'User',
+    fields: {
+        users: {
+            type: graphQLPage(userNodeType),
+            args: graphQLPageArgs,
+            resolve: (root, args: GraphQLPageArgs, context, fieldASTs) => {
+                return userGet(args)
+            }
+        }
+    }
 })
 
 export const userQuery = {
-	type: graphQLPage(userType),
-	args: graphQLPageArgs,
-	resolve: (root, args: GraphQLPageArgs, context, fieldASTs) => {
-		return userGet(args)
-	}
+    type: userType,
+    resolve: () => true
 }
 
 export const userMuation = {
-	userCreate: {
-		type: userType,
-		args: {
-			username: fieldCreate(GraphQLString, true),
-			email: fieldCreate(GraphQLString, true),
-			password: fieldCreate(GraphQLString, true)
-		},
-		resolve: (root, vars, context) => {
-			return userCreate(vars)
-		}
-	},
-	userDelete: {
-		type: userType,
-		args: {
-			_id: fieldCreate(GraphQLID, true)
-		},
-		resolve: (root, vars, context) => {
-			return userDelete(vars)
-		}
-	}
+    userCreate: {
+        type: userNodeType,
+        args: {
+            username: fieldCreate(GraphQLString, true),
+            email: fieldCreate(GraphQLString, true),
+            password: fieldCreate(GraphQLString, true)
+        },
+        resolve: (root, vars, context) => {
+            return userCreate(vars)
+        }
+    },
+    userDelete: {
+        type: userNodeType,
+        args: {
+            _id: fieldCreate(GraphQLID, true)
+        },
+        resolve: (root, vars, context) => {
+            return userDelete(vars)
+        }
+    }
 }
